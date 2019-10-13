5a1907e7382291702cf366e79e08fcb17a41f697
Shovon Das <shovondas26@gmail.com>
2019-10-13 18:40:08 +0600

    UI Updated

--- src/component/Join.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Chat = () => {
-  return (
-    <h1>Join</h1>
-  )
-}
-
-export default Chat;
---
+++ src/component/chat/Chat.js
@@ -0,0 +1,10 @@
+import React , { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Chat = () => {
+  return (
+    <h1>Chat</h1>
+  )
+}
+
+export default Chat;
